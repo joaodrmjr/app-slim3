4d51ccc4c45ef128878f1e68a91d5a7e5d686578
= <joaodrmjr@gmail.com>
2024-03-04 16:50:45 -0300

    Mostrando a faixa de premiacao no keno (javascript)

--- public/keno.js
+++ public/keno.js
@@ -1,8 +1,13 @@
 
 var numerosSelecionados = [];
 
-$("#tableNumeros td").click(function (e) {
+var faixaPremiacoes = {
+	1: ["1/3,96x"], 2: ["2/17,10x"], 3: ["3/81,5x"], 4: ["3/10x", "4/259x"], 5: ["3/4,5x", "4/48x", "5/450x"],
+	6: ["4/11x", "5/350x", "6/710x"], 7: ["4/7x", "5/90x", "6/400x", "7/800x"], 8: ["4/5x", "5/20x", "6/270x", "7/600x", "8/900x"],
+	9: ["4/4x", "5/11x", "6/56x", "7/500x", "8/800x", "9/1000x"], 10: ["4/3,5x", "5/8x", "6/13x", "7/63x", "8/500x", "9/800x", "10/1000x"]
+}
 
+$("#tableNumeros td").click(function (e) {
 
 	let num = parseInt($(this).attr("value"));
 
@@ -20,13 +25,39 @@ $("#tableNumeros td").click(function (e) {
 		numerosSelecionados.push(num);
 	}
 
+	let numsCount = numerosSelecionados.length;
 
-	console.log(numerosSelecionados);
+	mostraFaixa(numsCount);
 
 	if ($(this).hasClass("sel")) {
 		$(this).removeClass("sel");
 	}else {
 		$(this).addClass("sel");
 	}
-
 });
+
+
+function mostraFaixa(faixaNum) {
+
+	let table = $("#faixaPremiacoesTable");
+
+	if (faixaNum == 0) {
+		table.html("<tr><td colspan='2'>Selecione mais números</td></tr>")
+		return;
+	}
+	let content = "";
+	let faixa = faixaPremiacoes[faixaNum];
+
+	for (let i = 0; i < faixa.length; i++) {
+
+		let f = faixa[i].split("/");
+		content += "<tr>";
+		content += "<td>" + f[0] + "</td>";
+		content += "<td>" + f[1] + "</td>";
+		content += "</tr>";
+
+	}
+
+
+	table.html(content);
+}
